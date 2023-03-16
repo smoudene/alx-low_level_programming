94a8970ef8d96b4760ba6f4bcbb1e17c4e4562a0
smoudene <smoudene25@gmail.com>
2023-03-16 11:09:42 -0700

    fix 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,11 @@
 /**
  * main - Write String using Write
  *
- * Return: Always 0 (Success)
+ * Return: Return 1
  */
 int main(void)
 {
-	write(2, "and that piece uof art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
 }
