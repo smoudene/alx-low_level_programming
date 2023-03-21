4c42c65622397e0d8d992b2aa5a945eb4e6ec81b
smoudene <smoudene25@gmail.com>
2023-03-21 14:45:19 -0700

    fix 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,7 +14,7 @@ void print_times_table(int n)
 	i = 0;
 	if (n < 0 || n > 15)
 	{
-		return (0);
+		printf("");
 	}
 	else
 	{
@@ -23,7 +23,7 @@ void print_times_table(int n)
 			j = 0;
 			while (j <= n)
 			{
-				printf(j * i);
+				printf("%d", j * i);
 				if (j + 1 <= n && j + 1 >= 10)
 					printf(", ");
 				else if if (j + 1 <= n && j + 1 < 10)
