65fc35e67eb562f69bbdf84d4499baa86bcb80ca
smoudene <smoudene25@gmail.com>
2023-03-23 07:08:15 -0700

    first

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * int _isupper - function to check if char is uppercase
+ * @c : the char to be checked
+ *
+ * Return: 0
+ */
+int _isupper(int c)
+{
+	int a = 'A';
+
+	while (a <= 'Z')
+	{
+		if (c == a)
+		{
+			return (1);
+			break;
+		}
+		a++;
+	}
+	return (0);
+}
