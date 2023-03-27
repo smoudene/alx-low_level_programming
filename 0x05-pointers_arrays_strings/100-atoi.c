c29c0b28a48190379568ba630b0ad2ad0be03c45
smoudene <smoudene25@gmail.com>
2023-03-27 11:04:27 -0700

    check

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _atoi - ascii to integer 
+ * _atoi - ascii to integer
  * @s: string to be converted to integer
  * Return: 0 if there is no integer or the string
  */
@@ -18,30 +18,25 @@ int _atoi(char *s)
 
 	while (s[i])
 	{
-		if (s[j] >= '1' && s[j] <= '9')
+		if (s[i] >= '1' && s[i] <= '9')
 		{
 			no_num = 0;
 			break;
 		}
-		j++;
+		i++;
 	}
+	i = 0;
 
-	while (s[i] == ' ' || s[i] == '\n' || s[i] == '\t' || s[i] == '+')
+	while (s[i] == ' ' || s[i] == '\n' || s[i] == '\t' || s[i] == '-' ||
+			s[i] == '+'|| (s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
 	{
 		i++;
 	}
-	while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
-	{
-		i++;
-	}
-	if (s[i] == '-')
-	{
+	if (s[i - 1] == '-')
 		sign = -1;
-		i++;
-	}
 	while (s[i] && s[i] >= '0' && s[i] <= '9')
 	{
-		res = (res * 10) + (s[i] + '0');
+		res = (res * 10) + (s[i] - '0');
 		i++;
 	}
 	if (no_num == 1)
