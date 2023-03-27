5e25517c03defc3d1bcc81413f1dac1492ceafc5
smoudene <smoudene25@gmail.com>
2023-03-27 10:07:49 -0700

    mandatory done

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/*
+ * print_array - print n elements of an array
+ * @a: the array
+ * @n: number of elements
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (i < n)
+	{
+		printf("%d",a[i]);
+		if (i != n - 1)
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+}
