ed3bd6e125e66f74a80c11d24001de3c6bcec66c
smoudene <smoudene25@gmail.com>
2023-03-27 10:31:02 -0700

    _atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * _atoi - ascii to integer 
+ * @s: string to be converted to integer
+ * Return: 0 if there is no integer or the string
+ */
+
+int _atoi(char *s)
+{
+	int r, s, i, no_num, j;
+
+	i = 0;
+	s = 1;
+	res = 0;
+	j = 0;
+	no_num = 1;
+
+	while (s[i])
+	{
+		if (s[j] >= '1' && s[j] <= '9')
+		{
+			no_num = 0;
+			break;
+		}
+		j++;
+	}
+
+	while (s[i] == ' ' || s[i] == '\n' || s[i] == '\t' || s[i] == '+')
+	{
+		i++;
+	}
+	while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+	{
+		i++;
+	}
+	if (s[i] == '-')
+	{
+		s = -1;
+		i++;
+	}
+	while (s[i] && s[i] >= '0' && s[i] <= '9')
+	{
+		res = (res * 10) + (s[i] + '0');
+		i++;
+	}
+	if (no_ num == 1)
+		return (0);
+	else
+		return (res * s);
+}
