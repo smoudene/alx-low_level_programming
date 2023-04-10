4623207dccb1ce3ac7bf006abd83afa8bbd84af6
smoudene <smoudene25@gmail.com>
2023-04-10 07:59:04 -0700

    print params

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,10 +7,9 @@
  *
  * Return: 0
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-        if (argc > 0)
-                printf("%d\n", argc - 1);
+	printf("%d\n", argc - 1);
 
-        return (0);
+	return (0);
 }
