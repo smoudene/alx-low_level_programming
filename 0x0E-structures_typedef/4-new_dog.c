cc2ac2d5fdc9c062133d77b38547e64fc76ca34d
smoudene <smoudene25@gmail.com>
2023-04-17 08:21:57 -0700

    4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,53 @@
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: pointer to the new dog or  NULL
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+	int i = 0, j = 0;
+
+	while (name[i])
+		i++;
+	while (owner[j])
+		j++;
+
+	d = malloc(sizeof(dog_t));
+	if (!d)
+		return (NULL);
+	d->name = malloc(sizeof(char) * (i + 1));
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+	d->owner = malloc(sizeof(char) * (j + 1));
+	if (d->owner == NULL)
+	{
+		free(d);
+		free(name);
+		return (NULL);
+	}
+	i = 0;
+	j = 0;
+	while (name[i])
+	{
+		d->name[i] = name[i];
+		i++;
+	}
+	d->name[i] = '\0';
+	while (owner[j])
+	{
+		d->owner[j] = owner[j];
+		j++;
+	}
+	d->owner[j] = '\0';
+	d->age = age;
+	return (d);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,7 @@
 #define DOG_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
 * struct dog - a new type of dog
@@ -16,8 +17,10 @@ struct dog
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
