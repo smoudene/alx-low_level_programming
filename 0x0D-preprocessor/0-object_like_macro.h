5a1d88620a915fd9375c2bc552c8ba6431c6b8b2
smoudene <smoudene25@gmail.com>
2023-04-17 07:25:57 -0700

    fix 0 and 1

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,5 +1,5 @@
-#ifndef _0OBJECT_LIKE_MARCO_H
-#define 0_OBJECT_LIKE_MARCO_H
+#ifndef _0_OBJECT_LIKE_MARCO_H
+#define _0_OBJECT_LIKE_MARCO_H
 
 #define SIZE 1024
 
--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,5 +1,5 @@
 #ifndef _1_PI_H
-#define _1_Pi_H
+#define _1_PI_H
 
 #define PI 3.14159265359
 
