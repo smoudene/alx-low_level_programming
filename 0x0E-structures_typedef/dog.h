1ca0ff0dc28d3fa2ade56ad934d2eaa0db1ca573
smoudene <smoudene25@gmail.com>
2023-04-17 07:45:53 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - a new type of dog
+* @name: name of the dog dog
+* @age: age of the dog
+* @owner: owner of the dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
