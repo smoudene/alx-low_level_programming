bda8e488d798b533ab3328f12c1ed04a9f8e459d
smoudene <smoudene25@gmail.com>
2023-04-19 06:34:47 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTION_POINTERS_
+#define FUNCTION_POINTERS_
+
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+
+#endif
