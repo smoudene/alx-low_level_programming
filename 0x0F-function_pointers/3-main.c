692f7ca6185f1b39d961d7e7a1374e4bcae7fbd6
smoudene <smoudene25@gmail.com>
2023-04-19 07:16:16 -0700

    3operators

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,27 @@
+#include "3-calc.h"
+
+/**
+* main - program to test function op
+* @argc: argc
+* @argv: argv
+*
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int (*op)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	op = get_op_func(argv[2]);
+	if (!op)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", op(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
