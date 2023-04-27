80afed045354a751a3506b3d66ec4c494990f674
smoudene <smoudene25@gmail.com>
2023-04-27 07:28:46 -0700

    2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * add_node - add node begining of list
+ * @head: pointer to structure
+ * @str: string
+ * Return: new element address
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	unsigned int len = 0;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->str = strdup(str);
+	while (str[len] != '\0')
+		len++;
+	newN->len = len;
+	if (*head != NULL)
+		new->next = *head;
+	if (*head == NULL)
+		new->next = NULL;
+	*head = new;
+	return (*head);
+}
