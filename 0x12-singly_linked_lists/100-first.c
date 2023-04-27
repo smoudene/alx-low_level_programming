930d237069a2557ffdba4325763c87a0add01338
smoudene <smoudene25@gmail.com>
2023-04-27 07:54:57 -0700

    bonus part

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+/**
+* printBeforeMain - set the attribute constryctor
+* that allows to run a function before main
+*/
+void printBeforeMain(void) __attribute__((constructor));
+/**
+* printBeforeMain - prints a text
+*/
+void printBeforeMain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
