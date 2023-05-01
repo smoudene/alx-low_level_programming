ff2e9510bb95413ddb4fcf07f74d7a2174400320
smoudene <smoudene25@gmail.com>
2023-05-01 04:48:57 -0700

    fix n10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,27 +9,33 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *temp = *head;
-	listint_t *current = NULL;
-	unsigned int i = 0;
+	unsigned int cp = 1;
+	listint_t *new = *head, *temp;
 
-	if (*head == NULL)
+	if (!head || !*head)
 		return (-1);
+
 	if (index == 0)
 	{
-		*head = (*head)->next;
-		free(temp);
+		*head = new->next;
+		free(new);
 		return (1);
 	}
-	while (i < index - 1)
+
+	temp = *head;
+	while (temp)
 	{
-		if (!temp || !(temp->next)
-				return (-1);
-				temp = temp->next;
-				i++;
+		if (cp == index)
+		{
+			new = temp->next;
+			temp->next = new->next;
+			free(new);
+			return (1);
+		}
+
+		temp = temp->next;
+		cp++;
 	}
-	current = temp->next;
-	temp->next = current->next;
-	free(current);
-	return (1);
+
+	return (-1);
 }
